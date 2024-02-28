2010acaf6c6df083077077bfdd58091d1efa0b4a
jeanpaulmanirafasha90 <jeanpaulmanirafasha90@gmail.com>
2024-02-27 19:29:45 -0800

    modifying add

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,14 +1,17 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { Button, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Employees from './Employees';
 import {v4 as uuid} from "uuid";
 import {useNavigate} from 'react-router-dom'
 
+
 function Add(){
 
     const[name, setName] = useState('');
     const[age, setAge] = useState('');
+    const [nameInputColor, setNameInputColor] = useState('black')
+    const [errorMessage, setErrorMessage] = useState('')
 
     let history = useNavigate();
 
@@ -20,23 +23,35 @@ function Add(){
 
         let a = name,
         b = age;
+        if(a!='') {
+            Employees.push({id: uniqueId, Name : a, Age : b});
+            history("/");
+        }
+        setNameInputColor('red')
+        setErrorMessage('Fill the missing fields.')
 
-        Employees.push({id: uniqueId, Name : a, Age : b});
-
-        history("/");
     }
+
+    useEffect(()=>{
+        console.log('Something is happening!')
+    },[name])
 
     return(
         <div>
             <Form className="d-grid gap-2" style={{margin:"15rem"}}>
                 <Form.Group className="mb-3" controlId="formName">
-                    <Form.Control type="text" placeholder="Enter Name"  onChange={(e) => setName(e.target.value)} required>
+                    <Form.Control style={ {borderColor: nameInputColor}}  type="text" placeholder="Enter Name" required onChange={(e) => setName(e.target.value)}>
                     </Form.Control>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formAge">
                     <Form.Control type="text" placeholder="Enter Age" required onChange={(e) => setAge(e.target.value)}>
                     </Form.Control>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formAge">
+                    <div style={{ display: errorMessage==''? 'none': 'block', color:'red'}}>
+                        {errorMessage}
+                    </div>
                 </Form.Group>
                 <Button onClick={(e) => handleSubmit(e)} type="submit">Submit</Button>
             </Form>
