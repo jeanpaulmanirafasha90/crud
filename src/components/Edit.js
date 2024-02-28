6a70d1bf5d957d2f502ba7185469ab39d804fb7a
jeanpaulmanirafasha90 <jeanpaulmanirafasha90@gmail.com>
2024-02-27 18:19:26 -0800

    modifying aae

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 import { Button, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Employees from './Employees';
-import {v4 as uuid} from "uuid";
+//import {v4 as uuid} from "uuid";
 import {Link,useNavigate} from 'react-router-dom'
 
 function Edit(){
