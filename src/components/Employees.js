6753471ac98610ad5984c7db8bc35c172e2f9b82
jeanpaulmanirafasha90 <jeanpaulmanirafasha90@gmail.com>
2024-02-27 17:31:58 -0800

    adding components

---
+++ src/components/Employees.js
@@ -0,0 +1,17 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Jean",
+        Age:"25"
+    },{
+        id:"2",
+        Name:"Paul",
+        Age:"28"
+    },{
+        id:"3",
+        Name:"Aline",
+        Age:"20"
+    }
+]
+
+export default Employees;  
